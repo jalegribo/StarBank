c5bc185fe3d29a1c77e01e070bbe38c87068a0a8
Stars-Co <lushoalvarez45@gmail.com>
2021-08-08 11:24:04 -0500

    formulario de  datos (solo parte visual)

---
+++ app.js
@@ -0,0 +1,24 @@
+document.getElementById('natural').addEventListener('click', function(e) {
+    var nit= document.getElementById('nit').style.visibility='hidden';
+    var labelNit=document.getElementById('labelNit').style.visibility='hidden';
+    var nombreEmpresa=document.getElementById('nombreEmpresa').style.visibility='hidden';
+    var labelnombreParaEmpresa=document.getElementById('labelnombreParaEmpresa').style.visibility='hidden';
+    var nombreEmpresa=document.getElementById('nombreEmpresa').style.visibility='hidden';
+    var labelAreaComercial=document.getElementById('labelAreaComercial').style.visibility='hidden';
+    var areaComercial =document.getElementById('areaComercial').style.visibility='hidden';
+    var labelDireccionEmpresa =document.getElementById('labelDireccionEmpresa').style.visibility='hidden';
+    var direccionEmpresa=document.getElementById('direccionEmpresa').style.visibility='hidden';        
+});
+
+document.getElementById('juridica').addEventListener('click', function(e) {
+    var nit= document.getElementById('nit').style.visibility='visible';
+    var labelNit=document.getElementById('labelNit').style.visibility='visible';
+    var nombreEmpresa=document.getElementById('nombreEmpresa').style.visibility='visible';
+    var labelnombreParaEmpresa=document.getElementById('labelnombreParaEmpresa').style.visibility='visible';
+    var nombreEmpresa=document.getElementById('nombreEmpresa').style.visibility='visible';
+    var labelAreaComercial=document.getElementById('labelAreaComercial').style.visibility='visible';
+    var areaComercial =document.getElementById('areaComercial').style.visibility='visible';
+    var labelDireccionEmpresa =document.getElementById('labelDireccionEmpresa').style.visibility='visible';
+    var direccionEmpresa=document.getElementById('direccionEmpresa').style.visibility='visible';        
+
+});
